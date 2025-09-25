4e546728ca6862848e9e58841f8b13b496a490a0
Jesper Madsen <jesper.a.madsen@gmail.com>
2025-09-25 10:49:41 +0200

    does not compile

--- src/UmbracoProject/Hest.cs
+++ src/UmbracoProject/Hest.cs
@@ -4,6 +4,6 @@ public class Hest
 {
     public string HestNavn { get; set; } = "Hest";
     public string Type { get; set; } = "Pony";
-    do not compile!
+    do not compile! Even more do not compile
 
 }
