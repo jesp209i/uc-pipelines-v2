5ca2ba0e3548154b18fc0ebae0484dc7cde7874f
Jesper Madsen <jesper.a.madsen@gmail.com>
2025-09-25 10:53:49 +0200

    now

--- src/UmbracoProject/Hest.cs
+++ src/UmbracoProject/Hest.cs
@@ -3,7 +3,6 @@ namespace UmbracoProject;
 public class Hest
 {
     public string HestNavn { get; set; } = "Hest";
-    public string Type { get; set; } = "Pony";
-    do not compile! Even more do not compile
+    public string Type { get; set; } = "sony";
 
 }
