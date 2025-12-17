e3afe32d37f99c1187b4bea1044021e412a3367a
Jesper Madsen <jesper.a.madsen@gmail.com>
2025-12-17 13:49:02 +0100

    run the deployment yes

--- .github/scripts/start_deployment.sh
+++ .github/scripts/start_deployment.sh
@@ -13,7 +13,7 @@ pipelineVendor="$9"
 runSchemaExtraction="${10:-true}"
 skipPreserveUmbracoCloudJson="${11:-true}"
 
-dockerImageTag="0.3.86-beta"
+dockerImageTag="0.3.87-beta"
 
 
 # Not required, defaults to https://api.dev-cloud.umbraco.com
